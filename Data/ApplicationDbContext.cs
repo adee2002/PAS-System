f2b0a4b2e41455b64585c09cad2093a0c7966ffb
thirajnaveen <thirajnaveen@gmail.com>
2026-04-19 18:09:59 +0530

    Upload PAS System project

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<ResearchArea> ResearchAreas { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Match> Matches { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ResearchArea>()
+                .HasIndex(r => r.Name)
+                .IsUnique();
+
+            modelBuilder.Entity<Project>()
+                .HasOne(p => p.Student)
+                .WithMany()
+                .HasForeignKey(p => p.StudentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Project>()
+                .HasOne(p => p.ResearchArea)
+                .WithMany()
+                .HasForeignKey(p => p.ResearchAreaId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Match>()
+                .HasOne(m => m.Project)
+                .WithMany()
+                .HasForeignKey(m => m.ProjectId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Match>()
+                .HasOne(m => m.Supervisor)
+                .WithMany()
+                .HasForeignKey(m => m.SupervisorId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
